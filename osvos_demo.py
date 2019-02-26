9f63e6acb6aca9349d0b63ada1cac889c0b389d0
Unknown <dennis.groetzinger@web.de>
2019-02-26 15:22:57 +0100

    ready

--- osvos_demo.py
+++ osvos_demo.py
@@ -8,12 +8,6 @@ This file is part of the OSVOS paper presented in:
     CVPR 2017
 Please consider citing the paper if you use this code.
 
-@Inproceedings{Cae+17,
-  Title          = {One-Shot Video Object Segmentation},
-  Author         = {S. Caelles and K.K. Maninis and J. Pont-Tuset and L. Leal-Taix\'e and D. Cremers and L. {Van Gool}},
-  Booktitle      = {Computer Vision and Pattern Recognition (CVPR)},
-  Year           = {2017}
-}
 """
 
 
@@ -24,7 +18,7 @@ import collections
 
 import os
 import sys
-from PIL import Image
+from PIL import Image,ImageDraw
 import numpy as np
 import tensorflow as tf
 slim = tf.contrib.slim
@@ -38,7 +32,8 @@ os.chdir(root_folder)
 
 
 
-def rename(path):
+
+def rename(path,type):
     files = os.listdir(path)
     i = 0
     prefix = "0000"
@@ -54,7 +49,7 @@ def rename(path):
 
         elif( i >= 1000):
             prefix = "0"
-        os.rename(os.path.join(path, file), os.path.join(path, prefix + str(i)+'.jpg'))
+        os.rename(os.path.join(path, file), os.path.join(path, prefix + str(i)+'.' + type))
 
         i = i + 1
 
@@ -65,19 +60,69 @@ def rename(path):
 
 
 def getMiddle(left,right,bot,top):
-    x = left + (right - left)
-    y = top + (bot - top)
 
-    return(x,y)
 
+    x = left + (right - left)/2
+    y = top + (bot - top)/2
+
+
+    #if (image.getpixel((x,y))!= (255,255,255)):
+
+
+
+        #return getMiddle(left-1,right-1,bot,top,image)
+
+    return(int(x),int(y))
+
+
+def correction(x, y,image,index ):
+    xi = x
+    yi = y
+    if (index%8 == 0):
+        xi += 1 + index//8
+
+
+    elif (index % 8 == 1):
+        xi += -1 - index//8
+
+
+    #doesnt matter(7)
+    elif (index % 8 == 7):
+        yi +=1 + index//8
+
+    elif (index % 8 == 2):
+        yi += 1 - index//8
+
+    elif (index % 8 == 3):
+        xi += 1 + index//8
+        yi += 1 + index//8
+
+    elif (index % 8 == 4):
+        xi += -1 - index//8
+        yi += -1 - index//8
+
+    elif (index % 8 == 5):
+        xi += 1 + index//8
+        yi += -1 - index//8
+
+    elif (index % 8 == 6):
+        xi += -1 - index//8
+        yi += 1 + index//8
+    width, height = image.size
+    if (xi >= 0 and yi >= 0 and xi < width and yi < height and image.getpixel((xi,yi)) == (255,255,255)):
+        return (int(xi),int(yi))
+
+    if (index == 100):
+        return (0,0)
+    return correction(x,y,image,index+1)
 
 
 def convertToGraph(image):
     dict = {}
     width, height = image.size
     rgb_im = image.convert('RGB')
-    for x in range(width - 1):
-        for y in range(height - 1):
+    for x in range(width ):
+        for y in range(height ):
             dict[(x,y)] = []
             if (rgb_im.getpixel((x,y)) == (255,255,255)):
 
@@ -111,15 +156,16 @@ def convertToGraph(image):
 def writeLineToTxt(OutputPath,left,right,top,bot,start,finish,label):
     #start_offset end_offset RECTANGLE <id> <left> <top> <right> <bottom> [label]
     text_file = open(OutputPath, "w")
-    text_file.write(str(start) + " " + str(finish) + " " + "RECTANGLE" + " " + " " + "1" + " " + str(left) + " " + str(top) +" " +  str(right) + " " + str(bot) + " " + "[" + " " + str(label) + " " + "]" + "\n")
+    text_file.write(str(start) + "\t" + str(finish) + "\t" + "RECTANGLE" + "\t" + "1" + "\t" + str(left) + "\t" + str(top) +"\t" +  str(right) + "\t" + str(bot) + "\t" + str(label) + "\n")
     text_file.close()
 
 def returnString(left,right,top,bot,start,finish,label):
-    return str(start) + " " + str(finish) + " " + "RECTANGLE" + " " + " " + "1" + " " + str(left) + " " + str(top) +" " +  str(right) + " " + str(bot) + " " + "[" + " " + str(label) + " " + "]" + "\n"
+    return (str(start) + "\t" + str(finish) + "\t" + "RECTANGLE" + "\t" + "1" + "\t" + str(left) + "\t" + str(top) +"\t" +  str(right) + "\t" + str(bot) + "\t" + str(label) + "\n")
 
 
 def bfs(graph, root,visited):
     queue =  collections.deque([root])
+
     visited.add(root)
     while queue:
         vertex = queue.popleft()
@@ -148,26 +194,33 @@ def getMaxMin(leftShift,topShift,visited) :
 
 
 
+def getFirstMiddle(image):
+    width, height = image.size
 
-
+    for x in range(width - 1):
+        for y in range(height - 1):
+            if (image.getpixel((x,y)) == (255,255,255)):
+                return (x,y)
+    return (-1,-1)
 
 # User defined parameters
-seq_name = "2_01ReverseCut"
+seq_name = "fsGelbeAutos"
 gpu_id = 0
-train_model = False
+train_model = True
 result_path = os.path.join('DAVIS', 'Results', 'Segmentations', '480p', 'OSVOS', seq_name)
-middleX = 277
-middleY = 130
-rightShift = 473
-downShift = 572
+#middleX = 54
+#middleY = 120
+rightShift = 0 #- 5
+downShift = 0 #- 157
 
 
-rename(os.path.join('DAVIS', 'JPEGImages', '480p', seq_name))
+rename(os.path.join('DAVIS', 'JPEGImages', '480p', seq_name),"jpg")
+rename(os.path.join('DAVIS', 'Annotations', '480p', seq_name),"png")
 
 # Train parameters
 parent_path = os.path.join('models', 'OSVOS_parent', 'OSVOS_parent.ckpt-50000')
 logs_path = os.path.join('models', seq_name)
-max_training_iters = 80
+max_training_iters = 200
 
 # Define Dataset
 test_frames = sorted(os.listdir(os.path.join('DAVIS', 'JPEGImages', '480p', seq_name)))
@@ -223,31 +276,70 @@ for img_p in test_frames:
 result_frames = sorted(os.listdir(os.path.join('DAVIS', 'Results', 'Segmentations','480p','OSVOS', seq_name)))
 result_imgs = [os.path.join('DAVIS', 'Results','Segmentations', '480p','OSVOS', seq_name, frame) for frame in result_frames]
 
+
+
+
+
+png_im = Image.open(os.path.join('DAVIS', 'Annotations', '480p', seq_name,'00000.png'))
+
+(middleX,middleY) = getFirstMiddle(png_im)
+
+
+print(middleX,middleY)
 #initialize the results so that getMiddle does the right thing
-top = 0
+top = middleY
 bottom = middleY
-left = 0
+left = middleX
 right = middleX
 
 whites = set()
 strings = []
+counter =1
+amount = 0
+array = []
+for image in result_imgs:
+    amount += 1
+
 for image in result_imgs:
     im = Image.open(image)
     rgb_im = im.convert('RGB')
     dic = convertToGraph(rgb_im)
     (middleX,middleY) = getMiddle(left,right,bottom,top)
-    print(rgb_im.getpixel((middleX,middleY)))
-    print((middleX,middleY))
-    print((left, right, bottom, top))
+    if (rgb_im.getpixel((middleX,middleY))!= (255,255,255)):
+        (middleX,middleY) = correction(middleX,middleY,rgb_im,0)
+
+
+    #print(str((left, right, bottom, top)) +"  "+ str(counter))
+
     bfs(dic,(middleX,middleY),whites)
     (left, right, bottom, top) = getMaxMin(rightShift,downShift,whites)
+    whites = set()
+
     #writeLineToTxt(os.path.join('DAVIS', 'Annotations', '480p', seq_name,'text.txt'),left,right,top,bottom,1,1,'video')
-    strings.append(returnString(left,right,top,bottom,1,1,'video'))
+    strings.append(returnString(left,right,top,bottom,(-1)*(amount-counter)*100,(-1)*(amount - counter + 1)*100,'video'))
+    array.append([left,top,right,bottom])
+
+    counter = counter + 1
     left -= rightShift
     right -= rightShift
     top -= downShift
     bottom -= downShift
 
-text_file = open(os.path.join('DAVIS', 'Annotations', '480p', seq_name,'text.txt'), "w")
+text_file = open(os.path.join('DAVIS', 'Results', 'Segmentations','480p','OSVOS', seq_name,'AreaOfInterests.txt'), "w")
+#print("ok")
 text_file.writelines(strings)
 text_file.close()
+
+
+name = 0
+counter = 0
+for r_image in test_imgs:
+    r_image_opened = Image.open(r_image)
+    copy = r_image_opened.copy()
+    draw = ImageDraw.Draw(copy)
+    draw.rectangle(array[counter])
+
+    #Image.ImageDraw.Draw.rectangle(array[counter])
+    counter+=1
+    copy.save('C:/Users/Dennis/Documents/uni/Hiwi/VideoSeg/OSVOS-TensorFlow/DAVIS/ResultsWithRectangles/'+ seq_name + "/"+ str(name) + '.jpg')
+    name+=1
